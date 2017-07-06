6dd2e0d0078167b6cb5a57ed458e54b2a5aca442
marcinwojciak <wojciak.mr@gmail.com>
2017-07-06 17:04:02 +0200

    DataInitializer fix

--- eBookRental.Infrastructure/Services/DataInitializer.cs
+++ eBookRental.Infrastructure/Services/DataInitializer.cs
@@ -7,10 +7,12 @@ namespace eBookRental.Infrastructure.Services
     public class DataInitializer : IDataInitializer
     {
         private readonly IUserService _userService;
+        private readonly IBookService _bookService;
 
-        public DataInitializer(IUserService userService)
+        public DataInitializer(IUserService userService, IBookService bookService)
         {
             _userService = userService;
+            _bookService = bookService;
         }
 
         public async Task SeedDataAsync()
@@ -21,14 +23,6 @@ namespace eBookRental.Infrastructure.Services
                 var username = $"user{i}"; 
                 await _userService.RegisterAsync(userId, $"{username}@domain.com", 
                     username, $"{username}-{i}" ,"secret", "user", "1234567890987654", "123456789");
-            }
-
-            for (var i = 1; i <=5; i++)
-            {
-                var userId = Guid.NewGuid();
-                var username = $"admin{i}";
-                await _userService.RegisterAsync(userId, $"{username}@domain.com",
-                    username, $"{username}-{i}", "secret", "admin", "1234567890987654", "123456789");
             }
         }
     }
